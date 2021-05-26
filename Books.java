fe0054b3ca73a199efb294050b9068433f094a92
Luke Hyndman <luke.hyndman@student.onslow.school.nz>
2021-05-27 10:47:05 +1200

    Menu methods complete

--- Books.java
+++ Books.java
@@ -34,7 +34,33 @@ public class Books
         
         this.currId = 3;
     }
-
+    
+    public void addBook()
+    {
+        final int MAX_QUANTITY = 1000;
+        int quantity = -1;
+        
+        // ask user for the details 
+        String name = UI.askString("Title: ");
+        String author = UI.askString("Author: ");
+        
+        // Check boundaries for number of books added to stock
+        do 
+        {
+            quantity = UI.askInt("Quantity: ");
+        } while (0 > quantity || quantity > MAX_QUANTITY);
+        
+        // increment the book id counter and add book to hashmap
+        currId++;
+        booksMap.put(currId, new Book(currId, name, author, quantity));
+    }
+    
+    public void findBook()
+    {
+        int bookId = UI.askInt("Id: ");
+        UI.println(booksMap.get(bookId).getName());
+        
+    }
     public void printAll()
     {
         // traversing map
